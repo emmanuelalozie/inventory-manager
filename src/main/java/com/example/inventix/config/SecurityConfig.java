2beb2b633b4d6d4e5c96c0f9bba0e8f7280ccb32
emmanuelalozie <emmanuelalozie@github.com>
2024-11-05 00:41:21 -0800

    Roles defined, Spring Security ? Auth Disabled

--- src/main/java/com/example/inventix/config/SecurityConfig.java
+++ src/main/java/com/example/inventix/config/SecurityConfig.java
@@ -1,53 +1,52 @@
-package com.example.inventix.config;
-
-import com.example.inventix.model.Role;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/admin/**").hasRole(Role.ADMIN.name())
-                        .requestMatchers("/user/**").hasAnyRole(Role.USER.name(), Role.ADMIN.name())
-                        .requestMatchers("/public/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .formLogin(withDefaults())
-                .httpBasic(withDefaults());
-        return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        var userDetailsManager = new InMemoryUserDetailsManager();
-        userDetailsManager.createUser(User.withUsername("user")
-                .password(passwordEncoder().encode("userpass"))
-                .authorities(Role.USER.getAuthority())
-                .build());
-        userDetailsManager.createUser(User.withUsername("admin")
-                .password(passwordEncoder().encode("adminpass"))
-                .authorities(Role.ADMIN.getAuthority())
-                .build());
-        return userDetailsManager;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-}
+//package com.example.inventix.config;
+//
+//import com.example.inventix.model.Role;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import static org.springframework.security.config.Customizer.withDefaults;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("/admin/**").hasAuthority(Role.ADMIN.getAuthority())
+//                        .requestMatchers("/user/**").hasAnyAuthority(Role.USER.getAuthority(), Role.ADMIN.getAuthority())
+//                        .requestMatchers("/public/**").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .formLogin(withDefaults())
+//                .httpBasic(withDefaults());
+//        return http.build();
+//    }
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        var userDetailsManager = new InMemoryUserDetailsManager();
+//        userDetailsManager.createUser(User.withUsername("user")
+//                .password(passwordEncoder().encode("userpass"))
+//                .authorities(Role.USER.getAuthority())
+//                .build());
+//        userDetailsManager.createUser(User.withUsername("admin")
+//                .password(passwordEncoder().encode("adminpass"))
+//                .authorities(Role.ADMIN.getAuthority())
+//                .build());
+//        return userDetailsManager;
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//}
